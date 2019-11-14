c71170e8879be9320f51df0f6df346a02e7a3738
Goi Jia Jian <jiajian.goi.2018@sis.smu.edu.sg>
2019-11-14 19:02:21 +0800

    stuff

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,7 +1,7 @@
 const socket = io()
 let data = [];
 let chart = Morris.Line({
-    element: 'line-example',
+    element: 'chart-temperature',
     data,
     xkey: 'y',
     ykeys: ['a'],
@@ -20,6 +20,31 @@ socket.on('data-temperature', (content) => {
         y: content.time,
         a: content.data
     });
-    $('.table-body').append(template);
+    $('#table-temperature').append(template);
     chart.setData(data);
+});
+
+let data2 = [];
+let chart2 = Morris.Line({
+    element: 'chart-light',
+    data,
+    xkey: 'y',
+    ykeys: ['a'],
+    postUnits: '',
+    labels: ['Light'],
+    yLabelFormat: function (y) { return y.toString() + ''; },
+    parseTime: false,
+    pointFillColors: ['#ffffff'],
+    pointStrokeColors: ['gray'],
+    lineColors: ['red']
+});
+socket.on('data-light', (content) => {
+    let template = "<tr><td>" + content.data + "</td>" +
+        "<td>" + content.time + "</td> </tr> "
+    data2.push({
+        y: content.time,
+        a: content.data
+    });
+    $('#table-light').append(template);
+    chart2.setData(data2);
 });
